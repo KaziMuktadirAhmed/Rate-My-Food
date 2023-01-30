fa5b9494b0ee753c8f9328c2a0dd68ec04b293e3
KaziMuktadirAhmed <kazisujoy@gmail.com>
2023-01-30 17:37:04 +0600

    fix for searchbar layout ig

--- react-app/src/pages/Search/Search.tsx
+++ react-app/src/pages/Search/Search.tsx
@@ -7,27 +7,25 @@ import styles from "./Search.module.css";
 
 function Search() {
   return (
-    <>
-      <div className={styles.page_container}>
-        <header className={styles.page_header}>
-          <Topbar />
-        </header>
-        <main className={styles.page_content}>
-          <nav className={styles.content_navigation}>
-            <Navigation />
-          </nav>
-          <article className={styles.content_article}>
-            <Contents />
-          </article>
-          <aside className={styles.content_side_bar}>
-            <Sidebar />
-          </aside>
-        </main>
-        <footer className={styles.page_footer}>
-          <Footer />
-        </footer>
-      </div>
-    </>
+    <div className={styles.page_container}>
+      <header className={styles.page_header}>
+        <Topbar />
+      </header>
+      <main className={styles.page_content}>
+        <nav className={styles.content_navigation}>
+          <Navigation />
+        </nav>
+        <article className={styles.content_article}>
+          <Contents />
+        </article>
+        <aside className={styles.content_side_bar}>
+          <Sidebar />
+        </aside>
+      </main>
+      <footer className={styles.page_footer}>
+        <Footer />
+      </footer>
+    </div>
   );
 }
 
