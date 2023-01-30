4116f131becb5b304752c1b58a07c244f2f4ae92
KaziMuktadirAhmed <kazisujoy@gmail.com>
2023-01-30 16:36:12 +0600

    adds page level routing

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,42 +1,16 @@
-import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import Search from "./pages/Search/Search";
+import Temp from "./pages/temp/Temp";
+import Home from "./pages/Home/Home";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <Search />
-      <div className="topbarContainer">
-        <div className="topbarLeft">
-          <span className="logo">Freakbook</span>
-        </div>
-        <div className="topbarCenter">
-          <div className="searchbar">
-            &nbsp; &nbsp; Search &nbsp;
-            <input placeholder="Search for anything" className="searchInput" />
-          </div>
-        </div>
-        <div className="topbarRight">
-          <div className="topbarIcons">
-            <div className="topbarIconItem">
-              Person
-              <span className="topbarIconBadge">1</span>
-            </div>
-            <div className="topbarIconItem">
-              Chat
-              <span className="topbarIconBadge">1</span>
-            </div>
-            <div className="topbarIconItem">
-              Notification
-              <span className="topbarIconBadge">1</span>
-            </div>
-            <img src="" alt="" className="topbarImg" />
-          </div>
-        </div>
-      </div>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/Temp" element={<Temp />}></Route>
+      <Route path="/Search" element={<Search />}></Route>
+    </Routes>
   );
 }
 
